fa2d0eda16239138eb5ec75248c35c2ecbf1167d
Gabriel <gboeira777@gmail.com>
2023-03-03 23:32:14 -0300

    alterando para pt-br breeze

--- resources/lang/pt-BR/auth.php
+++ resources/lang/pt-BR/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
-    'password' => 'A senha fornecida está incorreta.',
-    'throttle' => 'Muitas tentativas de login. Tente novamente mais tarde'
+    'failed' => 'Essas credenciais nÃ£o foram encontradas em nossos registros.',
+    'password' => 'A senha informada estÃ¡ incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
 
 ];
