a01d8dbd82deaabfdadc6042bc46e9c1b3064c23
Gabriel <gboeira777@gmail.com>
2023-03-04 02:27:56 -0300

    Finalizando site para cadastro

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -69,7 +69,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center">
-          <h1 data-aos="fade-up">Nós somos a solução para seus agendamentos... <br> faça um teste!</h1>
+          <h1 data-aos="fade-up site-msg">Nós somos a solução para seus agendamentos... <br> faça um teste!</h1>
           <h2 data-aos="fade-up" data-aos-delay="400">Obtemos a melhor metodologia de agendamento para lavação de carros...</h2>
           <div data-aos="fade-up" data-aos-delay="600">
             <div class="text-center text-lg-start">
