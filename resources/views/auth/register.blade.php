37c7e6d170f186cf8fa8d54e16cb40d21b1a6a64
Gabriel <gboeira777@gmail.com>
2023-03-15 22:40:45 -0300

    inserindo criticas valida form cadastro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-auth-card>
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        {{-- <x-auth-validation-errors class="mb-4" :errors="$errors" /> --}}
 
         <form method="POST" action="{{ route('register') }}" style="width: 800px">
             @csrf
@@ -9,31 +9,36 @@
             <div class="row">
                 <div class="col-md-6">
                     <x-label for="name" :value="__('Name')" />
-                    <x-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus />
+                    <x-input id="name" class="form-control" type="text" name="name" :value="old('name')" autofocus />
+                    <small class="text-danger"> @error('name') {{ $message }} @enderror </small>
                 </div>
 
                 <div class="col-md-6">
                     <x-label for="email" :value="__('Email')" />
-                    <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required />
+                    <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" />
+                    <small class="text-danger"> @error('email') {{ $message }} @enderror </small>
                 </div>
             </div>
 
             <div class="row mt-4">
                 <div class="col-md-12">
                     <x-label for="business" :value="__('Nome do estabelecimento')" />
-                    <x-input id="business" class="form-control" type="business" name="business" :value="old('business')" required />
+                    <x-input id="business" class="form-control" type="business" name="business" :value="old('business')" />
+                    <small class="text-danger"> @error('business') {{ $message }} @enderror </small>
                 </div>
             </div>
 
             <div class="row mt-4">
                 <div class="col-md 6">
                     <x-label for="password" :value="__('Password')" />
-                    <x-input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
+                    <x-input id="password" class="form-control" type="password" name="password" autocomplete="new-password" />
+                    <small class="text-danger"> @error('password') {{ $message }} @enderror </small>
                 </div>
 
                 <div class="col-md-6">
                     <x-label for="password_confirmation" :value="__('Confirm Password')" />
-                    <x-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required />
+                    <x-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" />
+                    <small class="text-danger"> @error('password_confirmation') {{ $message }} @enderror </small>
                 </div>
             </div>
 
